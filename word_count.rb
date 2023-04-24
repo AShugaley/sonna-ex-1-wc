fd2c717824b8dea2c7142741d59b42c89bec5dfc
Alex Shugaley <shugaley@Alexs-MacBook-Air.local>
2023-04-24 15:04:31 -0500

    cosmetic

--- word_count.rb
+++ word_count.rb
@@ -5,52 +5,49 @@ class Phrase
 
   def initialize(phrase)
     self.phrase = phrase
-    self.word_count = {}
+    self.word_count = Hash.new(0)
 
-    clean!
-    count!
+    clean_phrase
+    count_words
   end
 
-  def print!
-    word_count.each do |word,cunt|
-      puts "#{word}: #{cunt}"
+  def print
+    word_count.each do |word, count|
+      puts "#{word}: #{count}"
     end
   end
 
   private
 
-  def clean!
-    remove_special_chars!
-    downcase!
-    handle_commas!
-    handle_apostrophes!
-    squish_whitespaces!
+  def clean_phrase
+    remove_special_chars
+    downcase
+    handle_commas
+    handle_apostrophes
+    squish_whitespaces
   end
   
-  def remove_special_chars!
+  def remove_special_chars
     self.phrase.gsub!(/[^a-zA-Z0-9\s',]/,'')
   end
 
-  def downcase!
+  def downcase
     self.phrase.downcase!
   end
 
-  def handle_commas!
+  def handle_commas
     self.phrase.gsub!(/,/,' ')
   end
 
-  def handle_apostrophes!
+  def handle_apostrophes
     self.phrase.gsub!(/(?<![a-z0-9])'|'(?![a-z0-9])/,'')
   end
 
-  def squish_whitespaces!
+  def squish_whitespaces
     self.phrase.gsub(/\s+/, ' ').strip
   end
 
-  def count!
-    phrase.split(" ").each do |word|
-      self.word_count[word] ||= 0
-      self.word_count[word] +=1
-    end
+  def count_words
+    phrase.split(" ").each { |word| word_count[word] +=1 }
   end
 end
